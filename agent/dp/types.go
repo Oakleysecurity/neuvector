a6c785eb03f65d4244c5bc276a3fb5a8c8ef4be0
0ak1ey <64719628+Oakleysecurity@users.noreply.github.com>
2023-05-03 17:54:43 +0800

    Update types.go

--- agent/dp/types.go
+++ agent/dp/types.go
@@ -6,7 +6,11 @@ import (
 	"github.com/neuvector/neuvector/share"
 )
 
+//##用于声明一个DP控制器中某些事件的回调函数
+//buf 表示一段字节切片
+//param 表示回调函数的其他参数
 type DPCallback func(buf []byte, param interface{}) bool
+//用于声明一个 DP 控制器中某些任务的回调函数
 type DPTaskCallback func(task *DPTask)
 
 const (
@@ -16,16 +20,16 @@ const (
 	DP_TASK_APPLICATION
 	DP_TASK_FQDN_IP
 )
-
-type Connection struct {
-	AgentID      string
+ 
+type Connection struct {   //包含了用于表示网络连接的各种信息字段
+	AgentID      string   //代理ID
 	HostID       string
-	ClientWL     string
-	ServerWL     string
+	ClientWL     string  //客户端白名单
+	ServerWL     string  //服务器白名单
 	ClientIP     net.IP
 	ServerIP     net.IP
-	Scope        string
-	Network      string
+	Scope        string  //表示连接的范围
+	Network      string  //表示连接所在的网络
 	ServerPort   uint16
 	ClientPort   uint16
 	IPProto      uint8
@@ -50,11 +54,11 @@ type Connection struct {
 }
 
 type ConnectionData struct {
-	EPMAC net.HardwareAddr
-	Conn  *Connection
+	EPMAC net.HardwareAddr  //表示设备mac地址
+	Conn  *Connection  //指向与该设备相关联的连接信息
 }
 
-type DPTask struct {
+type DPTask struct {   //定义了DP控制器中要执行的任务
 	Task     int
 	MAC      net.HardwareAddr
 	SecLog   *share.CLUSThreatLog
