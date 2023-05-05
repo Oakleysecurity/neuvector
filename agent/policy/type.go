bff579b2f5bada2e0b7f5b5ea48f8d479140fda0
0ak1ey <64719628+Oakleysecurity@users.noreply.github.com>
2023-05-05 20:13:58 +0800

    Update type.go

--- agent/policy/type.go
+++ agent/policy/type.go
@@ -26,17 +26,18 @@ type DlpBuildInfo struct {
 	ApplyDir     int
 }
 
+//##用于管理网络安全引擎的各个方面，包括IP策略、安全策略和数据泄漏防护规则等。同时，由于引擎需要处理多线程环境下的并发访问，因此使用互斥锁来保证其安全性。
 type Engine struct {
-	NetworkPolicy  map[string]*WorkloadIPPolicyInfo
-	ProcessPolicy  map[string]*share.CLUSProcessProfile
-	DlpWlRulesInfo map[string]*dp.DPWorkloadDlpRule
-	DlpBldInfo     *DlpBuildInfo
-	HostID         string
-	HostIPs        utils.Set
-	TunnelIP       []net.IPNet
-	Mutex          sync.Mutex
-	getGroupRule   GroupProcPolicyCallback
-	PolicyAddrMap  map[string]share.CLUSSubnet
+	NetworkPolicy  map[string]*WorkloadIPPolicyInfo  //一个map，用于存储工作负载级别的IP策略信息。key表示工作负载名称，value表示该工作负载的IP策略信息。
+	ProcessPolicy  map[string]*share.CLUSProcessProfile  //一个map，用于存储进程级别的安全策略信息。key表示进程编号，value表示该进程的安全策略信息。
+	DlpWlRulesInfo map[string]*dp.DPWorkloadDlpRule  //一个map，用于存储工作负载级别的数据泄漏防护规则信息。key表示工作负载名称，value表示该工作负载的数据泄漏防护规则信息。
+	DlpBldInfo     *DlpBuildInfo  //一个指向DLP（数据泄露防护）模块构建信息的指针。
+	HostID         string  //主机ID，表示当前主机的唯一标识符。
+	HostIPs        utils.Set  //一个集合，用于存储当前主机的所有IP地址。
+	TunnelIP       []net.IPNet  //一个IP地址列表，用于表示隧道连接的IP地址。
+	Mutex          sync.Mutex  //一个互斥锁，用于保证多线程环境下访问引擎对象的并发安全性。
+	getGroupRule   GroupProcPolicyCallback  //一个回调函数，用于获取进程组级别的安全策略信息。
+	PolicyAddrMap  map[string]share.CLUSSubnet  //一个map，用于存储子网名称和子网地址之间的映射关系。key表示子网名称，value表示子网地址。
 }
 
 func (e *Engine) Init(HostID string, HostIPs utils.Set, TunnelIP []net.IPNet, cb GroupProcPolicyCallback) {
